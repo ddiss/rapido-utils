76e7b561b1c1a67b804033970164ead0bf2e4899
David Disseldorp <ddiss@suse.de>
2020-09-15 14:01:17 +0200

    tcpdump: add simple tcpdump $RAPIDO_BR helper
    Signed-off-by: David Disseldorp <ddiss@suse.de>

---
+++ tcpdump/dump_rapido_br.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LLC 2020, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+RAPIDO_DIR="`dirname $0`/../rapido"
+. "${RAPIDO_DIR}/runtime.vars"
+
+outfile=$(mktemp --tmpdir="${RAPIDO_DIR}/initrds/" --suffix=".pcap")
+echo "dumping $BR_DEV to initrds/$(basename $outfile)"
+sudo tcpdump -i ${BR_DEV} -w $outfile
